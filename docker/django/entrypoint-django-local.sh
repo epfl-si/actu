ffffed0e07c082fc8bd7a26fe8a40c8135387831
William Belle <william.belle@gmail.com>
2026-05-05 14:13:38 +0200

    Configure Actu for debugging inside Docker

--- docker/django/entrypoint-django-local.sh
+++ docker/django/entrypoint-django-local.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 python src/manage.py migrate
-python src/manage.py runserver 0.0.0.0:8000
+python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:5678 src/manage.py runserver 0.0.0.0:8000

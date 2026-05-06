bc53a374a8004fb46049704521f57918c1617070
William Belle <william.belle@gmail.com>
2026-05-06 08:24:14 +0200

    Configs

--- src/configs/ocp.py
+++ src/configs/ocp.py
@@ -10,3 +10,15 @@ ALLOWED_HOSTS = ["*"]
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = "/public/static"
+
+
+# Behind a proxy
+# https://docs.djangoproject.com/en/5.2/ref/settings/#use-x-forwarded-host
+
+USE_X_FORWARDED_HOST = True
+
+
+# Custom HTTP header that tells Django whether the request came in via HTTPS
+# https://docs.djangoproject.com/en/5.2/ref/settings/#secure-proxy-ssl-header
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")

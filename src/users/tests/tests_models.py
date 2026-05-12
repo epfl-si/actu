d7722ee3d7aebfc5029664be05e69e4da4225159
Olivier Bieler <olivier.bieler@epfl.ch>
2026-05-12 14:30:38 +0200

    Move test file in a folder 'tests'

--- src/users/tests/tests_models.py
+++ src/users/tests/tests_models.py
@@ -7,11 +7,11 @@ User = get_user_model()
 
 class UserModelTest(TestCase):
     def test_create_user_with_sciper(self):
-        user = User.objects.create_user(username="test", sciper="99999999")
-        self.assertEqual(user.username, "test")
+        user = User.objects.create_user(username="niskanen", sciper="99999999")
+        self.assertEqual(user.username, "niskanen")
         self.assertEqual(user.sciper, "99999999")
 
     def test_sciper_is_unique(self):
-        User.objects.create_user(username="test", sciper="99999999")
+        User.objects.create_user(username="niskanen", sciper="99999999")
         with self.assertRaises(IntegrityError):
-            User.objects.create_user(username="test2", sciper="99999999")
+            User.objects.create_user(username="tomba", sciper="99999999")

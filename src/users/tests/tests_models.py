c432eb4d318b7ba04d39aebe6414a77ae9ad133d
Olivier Bieler <olivier.bieler@epfl.ch>
2026-05-13 09:16:04 +0200

    Create custom User model with EPFL SCIPER field (#25)

---
+++ src/users/tests/tests_models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+from django.db.utils import IntegrityError
+from django.test import TestCase
+
+User = get_user_model()
+
+
+class UserModelTest(TestCase):
+    def test_create_user_with_sciper(self):
+        user = User.objects.create_user(username="niskanen", sciper="99999999")
+        self.assertEqual(user.username, "niskanen")
+        self.assertEqual(user.sciper, "99999999")
+
+    def test_sciper_is_unique(self):
+        User.objects.create_user(username="niskanen", sciper="99999999")
+        with self.assertRaises(IntegrityError):
+            User.objects.create_user(username="tomba", sciper="99999999")

2abfa18b16a516a68dc44c5e6647fb6fe1ca1ae9
Rosa Maggi <rosa.maggi@epfl.ch>
2026-05-21 12:39:16 +0200

    Add `languages` model

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     # Applications
     "homepages",
     "users",
+    "languages"
 ]
 
 MIDDLEWARE = [
---
+++ src/languages/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Language(models.Model):
+    """
+    Language is the list of available languages
+
+    For example : en, fr
+    """
+    language = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.language
+
+

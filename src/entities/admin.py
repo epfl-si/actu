a3768579f78147cd72411ea2cbc8e417d6e643be
antoine <antoine.fabre@epfl.ch>
2026-05-29 11:18:47 +0200

    [fix] Add label_en in django admin entities

--- src/entities/admin.py
+++ src/entities/admin.py
@@ -5,4 +5,4 @@ from .models import Entity
 
 @admin.register(Entity)
 class EntityAdmin(admin.ModelAdmin):
-    list_display = ("is_active", "is_main", "has_homepage")
+    list_display = ("label_en", "is_active", "is_main", "has_homepage")

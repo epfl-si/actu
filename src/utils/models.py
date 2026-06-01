c93121979505ccffab69da05d5ddadf9002c4108
antoine <antoine.fabre@epfl.ch>
2026-06-01 12:08:16 +0200

    [fix] Update utils model

--- src/utils/models.py
+++ src/utils/models.py
@@ -3,12 +3,19 @@ from django.db import models
 
 
 class LabelModel(models.Model):
-    label_fr = models.CharField(max_length=100)
-    label_en = models.CharField(max_length=100)
-    label_de = models.CharField(max_length=100)
-    label_it = models.CharField(max_length=100)
+    label_fr = models.CharField(max_length=200)
+    label_en = models.CharField(max_length=200)
+    label_de = models.CharField(max_length=200)
+    label_it = models.CharField(max_length=200)
 
-    def str(self):
+    search_fields = [
+        "label_en",
+        "label_fr",
+        "label_de",
+        "label_it",
+    ]
+
+    def __str__(self):
         lang = utils.translation.get_language()
         return self.__getattribute__("label_" + lang)
 

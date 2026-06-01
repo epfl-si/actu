1b870af129f8d1c56f7cf43b80fcb6bef8145f65
William Belle <william.belle@gmail.com>
2026-06-01 19:02:29 +0200

    [fix] Revert entities and thematics models"
    This reverts commit 4f5d7aa9639eb55bf6e7616e6e285ac3928924ed.

--- src/entities/models.py
+++ src/entities/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from utils.models import LabelModel
 
@@ -11,33 +12,35 @@ class Entity(LabelModel):
     """
 
     class Meta:
-        verbose_name = "Entity"
-        verbose_name_plural = "Entities"
+        verbose_name = _("Entity")
+        verbose_name_plural = _("Entities")
 
     is_active = models.BooleanField(
         default=True,
-        verbose_name=("Active"),
-        help_text=(
+        verbose_name=_("Active"),
+        help_text=_(
             "Designates whether this entity is active and visible in "
             "the system."
         ),
     )
     is_main = models.BooleanField(
         default=False,
-        verbose_name=("Main"),
-        help_text=(
+        verbose_name=_("Main"),
+        help_text=_(
             "Designates whether this entity is displayed on the main page."
         ),
     )
     has_homepage = models.BooleanField(
         default=False,
-        verbose_name=("Has Homepage"),
-        help_text=("Designates whether this entity has a dedicated homepage."),
+        verbose_name=_("Has Homepage"),
+        help_text=_(
+            "Designates whether this entity has a dedicated homepage."
+        ),
     )
     order = models.PositiveIntegerField(
         default=0,
-        verbose_name=("Order"),
-        help_text=(
+        verbose_name=_("Order"),
+        help_text=_(
             "Defines the display order. A value of 0 means that the order "
             "is ignored (for non-main entities)."
         ),
--- src/thematics/models.py
+++ src/thematics/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from utils.models import LabelModel
 
@@ -12,30 +13,30 @@ class Thematic(LabelModel):
 
     is_active = models.BooleanField(
         default=True,
-        verbose_name=("Active"),
-        help_text=(
+        verbose_name=_("Active"),
+        help_text=_(
             "Designates whether this thematic is active and visible in "
             "the system."
         ),
     )
     is_main = models.BooleanField(
         default=False,
-        verbose_name=("Main"),
-        help_text=(
+        verbose_name=_("Main"),
+        help_text=_(
             "Designates whether this thematic is displayed on the main page."
         ),
     )
     has_homepage = models.BooleanField(
         default=False,
-        verbose_name=("Has Homepage"),
-        help_text=(
+        verbose_name=_("Has Homepage"),
+        help_text=_(
             "Designates whether this thematic has a dedicated homepage."
         ),
     )
     order = models.PositiveIntegerField(
         default=0,
-        verbose_name=("Order"),
-        help_text=(
+        verbose_name=_("Order"),
+        help_text=_(
             "Defines the display order. A value of 0 means the thematic "
             "is hidden."
         ),

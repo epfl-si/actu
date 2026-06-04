e9f3274f93e4b95a429b218c06bf5fdf5fd67d9d
antoine <antoine.fabre@epfl.ch>
2026-06-04 10:26:25 +0200

    [fix] format

--- src/thematics/context_processors.py
+++ src/thematics/context_processors.py
@@ -1,11 +1,12 @@
 from .models import Thematic
 
+
 def global_thematics(request):
     try:
-        active_thematics = Thematic.objects.filter(is_active=True).order_by('order')
+        active_thematics = Thematic.objects.filter(is_active=True).order_by(
+            "order"
+        )
     except Exception:
         active_thematics = []
 
-    return {
-        "thematics": active_thematics
-    }
+    return {"thematics": active_thematics}

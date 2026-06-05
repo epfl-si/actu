463c54867086e5f170c6f1c8560b788117077e6f
antoine <antoine.fabre@epfl.ch>
2026-06-05 10:40:44 +0200

    [fix] Sort alphabetical drop down

--- src/thematics/context_processors.py
+++ src/thematics/context_processors.py
@@ -3,10 +3,21 @@ from .models import Thematic
 
 def global_thematics(request):
     try:
-        active_thematics = Thematic.objects.filter(is_active=True).order_by(
-            "order"
-        )
-    except Exception:
-        active_thematics = []
+        qs = Thematic.objects.filter(is_active=True).order_by("order")
 
-    return {"thematics": active_thematics}
+        main_thematics = [t for t in qs if t.is_main]
+
+        other_thematics = sorted(
+            [t for t in qs if not t.is_main],
+            key=lambda t: str(t).lower()
+        )
+
+
+    except Exception:
+        main_thematics = []
+        other_thematics = []
+
+    return {
+        "main_thematics": main_thematics,
+        "other_thematics": other_thematics,
+    }

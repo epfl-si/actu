9a5b54b35e2bb56c73b60083f0f1387faf39a341
Olivier Bieler <olivier.bieler@epfl.ch>
2026-06-16 16:00:17 +0200

    Improve User admin and display (#42)

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = (
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "sciper",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+        "last_login",
+    )
+
+    readonly_fields = ["last_login", "date_joined"]
+
+    search_fields = ["username", "first_name", "last_name", "email", "sciper"]
+
+    list_display = [
+        "username",
+        "first_name",
+        "last_name",
+        "email",
+        "sciper",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    ]
+
+    list_filter = ["is_active", "is_staff", "is_superuser"]
+
+    ordering = ["-date_joined"]
--- src/users/models.py
+++ src/users/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class User(AbstractUser):
     sciper = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.sciper})"

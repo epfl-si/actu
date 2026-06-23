e87e48f969c256099b1574c58d36feaf8911958d
antoine <antoine.fabre@epfl.ch>
2026-06-23 10:11:35 +0200

    [fix] code format

--- src/homepages/urls.py
+++ src/homepages/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import homepages, HomepageUsersManageView
+from .views import HomepageUsersManageView, homepages
 
 urlpatterns = [
     path("", homepages, name="homepages"),
-    path('admin/homepage/<int:pk>/manage/', HomepageUsersManageView.as_view(), name='manage_homepage_users'),
+    path(
+        "admin/homepage/<int:pk>/manage/",
+        HomepageUsersManageView.as_view(),
+        name="manage_homepage_users",
+    ),
 ]
--- src/homepages/views.py
+++ src/homepages/views.py
@@ -1,12 +1,13 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import DetailView
-from django.contrib.auth.mixins import UserPassesTestMixin
+
 from .models import Homepage
 
 
 class HomepageUsersManageView(UserPassesTestMixin, DetailView):
     model = Homepage
-    template_name = 'homepages/manage_users.html'
+    template_name = "homepages/manage_users.html"
 
     def test_func(self):
         return self.request.user.is_staff

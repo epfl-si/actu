d6f2361c96d043631c8fa15c4e0bdd80ff352528
antoine <antoine.fabre@epfl.ch>
2026-06-23 11:24:06 +0200

    [feature] Add user list

--- src/homepages/views.py
+++ src/homepages/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render
 from django.views.generic import DetailView
 
 from .models import Homepage
+
+User = get_user_model()
 
 
 class HomepageUsersManageView(UserPassesTestMixin, DetailView):
@@ -12,9 +15,16 @@ class HomepageUsersManageView(UserPassesTestMixin, DetailView):
     def test_func(self):
         return self.request.user.is_staff
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        homepage = self.get_object()
+
+        context["current_users"] = homepage.users.all()
+
+        return context
+
 
 def homepages(request):
-
     return render(
         request,
         "home.html",

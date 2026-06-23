1b63be2faac4ef9ac37085f549f2c5ff930398c7
antoine <antoine.fabre@epfl.ch>
2026-06-23 09:55:03 +0200

    [feature] Add simple view

--- src/homepages/views.py
+++ src/homepages/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from .models import Homepage
+
+
+class HomepageUsersManageView(UserPassesTestMixin, DetailView):
+    model = Homepage
+    template_name = 'homepages/manage_users.html'
+
+    def test_func(self):
+        return self.request.user.is_staff
 
 
 def homepages(request):

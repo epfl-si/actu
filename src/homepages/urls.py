06051102e8f1c26f229ae8b65430060c42a20e6e
antoine <antoine.fabre@epfl.ch>
2026-06-23 09:53:17 +0200

    [feature] Add url for this view

--- src/homepages/urls.py
+++ src/homepages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import homepages
+from .views import homepages, HomepageUsersManageView
 
 urlpatterns = [
     path("", homepages, name="homepages"),
+    path('admin/homepage/<int:pk>/manage/', HomepageUsersManageView.as_view(), name='manage_homepage_users'),
 ]

127d619caf4a12ecea8db00e92f0a8fac4ecc08d
Olivier Bieler <olivier.bieler@epfl.ch>
2026-06-23 10:19:02 +0200

    Save update_by on translations admin

--- src/translations/admin.py
+++ src/translations/admin.py
@@ -47,4 +47,6 @@ class NewsTranslationAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.created_by = request.user
+        else:
+            obj.updated_by = request.user
         super().save_model(request, obj, form, change)

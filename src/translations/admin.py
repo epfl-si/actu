dce6b963b9f986b31f5a61fb1f60e1ef64ff4ad2
Olivier Bieler <olivier.bieler@epfl.ch>
2026-06-23 13:48:10 +0200

    Fix N+1 queries on News

--- src/translations/admin.py
+++ src/translations/admin.py
@@ -33,17 +33,6 @@ class NewsTranslationAdmin(admin.ModelAdmin):
         "published_by",
     ]
 
-    def get_queryset(self, request):
-        return (
-            super()
-            .get_queryset(request)
-            .select_related(
-                "created_by",
-                "updated_by",
-                "published_by",
-            )
-        )
-
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.created_by = request.user

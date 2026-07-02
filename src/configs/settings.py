569211613a7a7abbab4e1910ca799fd6eb72cf4d
William Belle <william.belle@gmail.com>
2026-07-02 15:03:43 +0200

    Clean

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static_root/"
+STATIC_ROOT = BASE_DIR / "static_root"
 
 STATIC_URL = "static/"
 

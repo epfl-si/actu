52898836320ba09d4ac9655bdb5f96517d913abc
Olivier Bieler <olivier.bieler@epfl.ch>
2026-08-04 15:38:58 +0200

    Fix js codes

--- src/assets/pages/manage-homepages/manage-homepages.js
+++ src/assets/pages/manage-homepages/manage-homepages.js
@@ -1,17 +1,17 @@
 $(function () {
-  var $formToSubmit = null;
+  let $formToSubmit = null
 
   $('#confirm_delete').on('show.bs.modal', function (event) {
-    var $button = $(event.relatedTarget);
-    $formToSubmit = $button.closest('form');
+    const $button = $(event.relatedTarget)
+    $formToSubmit = $button.closest('form')
 
-    $('#confirm-delete-homepage').text($button.data('homepage'));
-    $('#confirm-delete-lang').text($button.data('lang'));
-  });
+    $('#confirm-delete-homepage').text($button.data('homepage'))
+    $('#confirm-delete-lang').text($button.data('lang'))
+  })
 
   $('#confirm-delete-submit').on('click', function () {
     if ($formToSubmit) {
-      $formToSubmit.submit();
+      $formToSubmit.submit()
     }
-  });
-});
+  })
+})

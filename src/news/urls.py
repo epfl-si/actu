146a06827608f58b415cc6b8b92e018e107b86a8
lvenries <luc.venries@epfl.ch>
2026-08-07 14:29:04 +0200

    [feature] add new page : create-news

--- src/news/urls.py
+++ src/news/urls.py
@@ -5,6 +5,11 @@ from . import views
 urlpatterns = [
     path("news/manage/", views.manage_news, name="manage_news"),
     path(
+        "news/create/",
+        views.create_news,
+        name="create_news",
+    ),
+    path(
         "news/manage/<int:news_id>/<str:lang>/delete/",
         views.delete_news_translation,
         name="delete_news_translation",

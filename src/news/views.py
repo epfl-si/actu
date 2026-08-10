e95b996e5799a79e25b20172ae439b832a9fa45d
Rosa Maggi <rosa.maggi@epfl.ch>
2026-08-10 17:20:09 +0200

    Save a new news

--- src/news/views.py
+++ src/news/views.py
@@ -18,9 +18,41 @@ from .models import News
 from translations.models import NewsTranslation
 from thematics.models import Thematic
 from entities.models import Entity
+from .forms import NewsForm
 
 
 User = get_user_model()
+
+def _handle_post_action(request):
+    action = request.POST.get("action")
+
+    if action == "add":
+        thematic = request.POST.getlist("thematics")
+        if not thematic:
+            messages.error(request, _("No thematic provided."))
+            return
+        entity = request.POST.getlist("entities")
+        if not entity:
+            messages.error(request, _("No entity provided."))
+            return
+
+        print(entity)
+        news_form = NewsForm(request.POST)
+
+        if news_form.is_valid():
+            news = news_form.save(commit=False)
+            news.created_by = request.user
+            news.save()
+            news_form.save_m2m()
+            messages.success(
+                request,
+                _("The news %(title) has been added successfully.")
+                % {"title": "test"},
+                )
+            return True
+        else:
+            logger.error("error saving")
+            return False
 
 
 @login_required
@@ -42,6 +74,13 @@ def create_news(request):
         { 'code': 'de', 'label': 'German version', 'active': False },
         { 'code': 'it', 'label': 'Italian version', 'active': False }
     ]
+
+    if request.method == "POST":
+        created = _handle_post_action(request)
+        if created:
+            return redirect("create_news")
+    else:
+        form = NewsForm()
 
     context = {
         "news_var": news_var,

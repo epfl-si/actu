ed0e047540a56ece4dfc9b19a09b5d1162c97905
Rosa Maggi <rosa.maggi@epfl.ch>
2026-08-18 11:40:30 +0200

    Add other fields

--- src/news/forms.py
+++ src/news/forms.py
@@ -33,7 +33,7 @@ class NewsForm(forms.ModelForm):
 class NewsTranslationForm(forms.ModelForm):
     class Meta:
         model = NewsTranslation
-        fields = ['title', 'hat']
+        fields = ['title', 'hat', 'extract', 'author', 'funding', 'references']
 
     def save(self, request, language, news_id):
         is_new = self.instance.pk is None

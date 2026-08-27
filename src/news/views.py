ed1f99eea567455609397fca23acfa7909ee719b
Rosa Maggi <rosa.maggi@epfl.ch>
2026-08-27 08:52:24 +0200

    Translate validations

--- src/news/views.py
+++ src/news/views.py
@@ -1,7 +1,6 @@
 from django import utils
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
 from django.db import IntegrityError, transaction

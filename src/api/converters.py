5f37b04208a98d7c1c17245a72a0dc68736c696f
William Belle <william.belle@gmail.com>
2026-08-28 19:53:43 +0200

    Escape API version strings with re.escape

--- src/api/converters.py
+++ src/api/converters.py
@@ -1,8 +1,12 @@
+import re
+
 from django.conf import settings
 
 
 class APIVersionConverter:
-    regex = "|".join(settings.REST_FRAMEWORK["ALLOWED_VERSIONS"])
+    regex = "|".join(
+        re.escape(v) for v in settings.REST_FRAMEWORK["ALLOWED_VERSIONS"]
+    )
 
     def to_python(self, value):
         return value

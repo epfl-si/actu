c1961d4c712591aef36de40293199937bc5f3880
Rosa Maggi <rosa.maggi@epfl.ch>
2026-09-03 10:46:34 +0200

    Fix imports

--- src/news/views.py
+++ src/news/views.py
@@ -2,11 +2,8 @@ from django import utils
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.core.exceptions import PermissionDenied
-from django.db import IntegrityError, transaction
 from django.core.paginator import Paginator
 from django.db.models import Prefetch
-from django.http import Http404, JsonResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse

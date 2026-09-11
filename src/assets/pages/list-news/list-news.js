dcf75f296a55374f0fcfbbb94da96f8b1ae5b03f
antoine <antoine.fabre@epfl.ch>
2026-09-11 07:56:31 +0200

    [remove] Colapse

--- src/assets/pages/list-news/list-news.js
+++ src/assets/pages/list-news/list-news.js
@@ -1,29 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('news-form')
-  const filtersZone = document.getElementById('collapse-1')
   const btnFilter = document.getElementById('btnFilter')
 
-  if (!form || !filtersZone || !btnFilter) return
+  if (!form || !btnFilter) return
 
   let hasChanged = false
 
-  filtersZone.addEventListener('change', () => { hasChanged = true }, true)
+  if (window.$) {
+    $('.select-multiple').on('change', function () {
+      hasChanged = true
+    })
 
-  filtersZone.addEventListener('focusout', function () {
-    setTimeout(function () {
-      if (hasChanged && !filtersZone.contains(document.activeElement)) {
+    $(document).on('click', function (e) {
+      if (hasChanged && !$(e.target).closest('.form-group').length) {
         hasChanged = false
         btnFilter.click()
       }
-    }, 0)
-  })
+    })
 
-  if (window.$) {
     $('.filter-remove').on('click', function (e) {
       e.preventDefault()
+
       const type = $(this).data('type')
       const val = $(this).data('val')
-      $('#' + type).find(`option[value="${val}"]`).prop('selected', false).trigger('change')
+
+      $('#' + type)
+        .find(`option[value="${val}"]`)
+        .prop('selected', false)
+        .trigger('change')
+
       btnFilter.click()
     })
   }

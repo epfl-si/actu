b408e9b2ec0380918db7cc6876532376629d8421
Rosa Maggi <rosa.maggi@epfl.ch>
2026-09-14 13:00:10 +0200

    Fix empty links when creating

--- src/news/views.py
+++ src/news/views.py
@@ -13,6 +13,7 @@ from django.utils.translation import gettext_lazy as _
 from django.views.decorators.http import require_POST
 
 from entities.models import Entity
+from multi_ref.models import NewsMultiRef
 from news_formats.models import NewsFormat
 from thematics.models import Thematic
 from translations.models import NewsTranslation
@@ -147,7 +148,9 @@ def _initialize_form_and_render_view(request, lang, news_id=None):
 
     news = get_object_or_404(News, id=news_id) if news_id else None
     translation = news.get_translation(language=lang) if news else None
-    links = news.get_links(language=lang) if news else None
+    links = (
+        news.get_links(language=lang) if news else NewsMultiRef.objects.none()
+    )
 
     form = NewsWithTranslationForm(
         post_data=request.POST or None,
